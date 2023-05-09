1f0017227d6c0ee44b9fbdea06be2becd71ec691
Your Name <94569213+chronos15@users.noreply.github.com>
2023-05-09 09:20:00 -0300

    123

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+const apiUrl = 'http://192.168.1.200:3040/SOFTWORKMOBILE/Imagens';
+
+// endpoint para listar os nomes das imagens
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get(apiUrl);
+    const images = response.data.split('\n')
+      .filter(item => item.includes('.jpg'))
+      .map(item => item.match(/href="(.*)"/)[1]);
+    res.send(images);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
+  }
+});
+
+// endpoint para retornar a imagem pelo nome
+app.get('/:imageName', async (req, res) => {
+  const imageName = req.params.imageName;
+  const imageUrl = `${apiUrl}/${imageName}`;
+  try {
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    res.set('Content-Type', 'image/jpeg');
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error');
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
